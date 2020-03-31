4d6e1e968b2e13db13e8ed7e87feb6f045175213
Praveen Chandran <pc-mac@Praveens-MacBook-Air.local>
2020-03-31 12:31:55 +0400

    adding all the files

---
+++ build/precache-manifest.bb8a2aa129e8ee3249191fea0825e24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0590316bd7536cac624f6a934dc84ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e458054ed805b0c27c95",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3e7630225d30e0bca29",
+    "url": "/static/js/2.e4babcec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4babcec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e458054ed805b0c27c95",
+    "url": "/static/js/main.316f7592.chunk.js"
+  },
+  {
+    "revision": "260f5a634f0624d4e0f7",
+    "url": "/static/js/runtime-main.724b5037.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
